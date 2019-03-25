0a99b0700a6612b90b29f6b200678489c8c2135a
y-akamatsu <ak1985tk@gmail.com>
2019-03-25 21:00:01 +0900

    create let_const

--- beginner_lesson/src/Lesson_LetConst.ts
+++ beginner_lesson/src/Lesson_LetConst.ts
@@ -1,12 +1,11 @@
 export function Lesson_LetConst() {
   let count: number = 1
   count += 1
-  //変数と定数：　変数は再代入可能、定数は再代入不可
-  let message: string = ''
+
+  let  message: string = ''
   message = `Lesson ${count}`
   console.log("変数", message)
 
   const name: string = "山田太郎"
   console.log("定数", name)
 }
-Lesson_LetConst()
