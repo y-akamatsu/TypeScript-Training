8d4df99cc73e46f0e9c7c19a405d85775209d92a
y-akamatsu <ak1985tk@gmail.com>
2019-03-25 21:42:32 +0900

    create lesson_PrinAny

---
+++ beginner_lesson/src/Lesson_PrinAny.ts
@@ -0,0 +1,24 @@
+import { AnyARecord } from "dns";
+
+export function Lesson_PrinAny() {
+  const num: number = 10
+  const str: string = "文字列"
+  const isActive: boolean = true
+
+  console.log("num", num) //数字
+  console.log("str", str) //文字列
+  console.log("isActive", isActive) //真偽値
+
+  let variable: any
+  variable = 1
+  console.log("number", variable)
+
+  variable = "文字列"
+  console.log("string", variable)
+
+  variable = false
+  console.log("boolean", variable)
+
+  // variable = new Data()
+  // console.log("class", variable)
+}
--- beginner_lesson/src/app.ts
+++ beginner_lesson/src/app.ts
@@ -1,19 +1,9 @@
-// import { Lesson_LetConst } from './lesson_LetConst';
+import { Lesson_LetConst } from './lesson_LetConst';
 import { Lesson_Function } from './Lesson_Function';
+import { Lesson_PrinAny } from './Lesson_PrinAny';
 
-// Lesson_LetConst();
-Lesson_Function();
+Lesson_LetConst()
+Lesson_Function()
+Lesson_PrinAny()
 
-// export function Lesson_LetConst() {
-//   let count: number = 1
-//   count += 1
-//   //変数と定数：　変数は再代入可能、定数は再代入不可
-//   let message: string = ''
-//   message = `Lesson ${count}`
-//   console.log("変数", message)
-
-//   const name: string = "山田太郎"
-//   console.log("定数", name)
-// }
-// Lesson_LetConst()
 
