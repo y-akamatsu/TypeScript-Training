5b686dc178500fe14698546a952cbb845c80cdbb
y-akamatsu <ak1985tk@gmail.com>
2019-03-25 22:30:02 +0900

    create lesson_Optinal

---
+++ beginner_lesson/src/Lesson_Optional.ts
@@ -0,0 +1,15 @@
+export function Lesson_Optional(){
+  console.log("result1", Lesson4_1("shohei", "developer"))
+  console.log("result1", Lesson4_1("shohei", undefined))
+  console.log("result1", Lesson4_1("shohei", null))
+}
+
+function Lesson4_1(name: string, profile: string): string {
+  if(!profile){
+    return `profileが${profile}です`
+  }
+  return name + "は" + profile
+}
+
+//処理の流れが理解できていない
+//! ←　エクスクラメーションマークは否定文
--- beginner_lesson/src/app.ts
+++ beginner_lesson/src/app.ts
@@ -1,9 +1,11 @@
-import { Lesson_LetConst } from './lesson_LetConst';
-import { Lesson_Function } from './Lesson_Function';
-import { Lesson_PrinAny } from './Lesson_PrinAny';
+// import { Lesson_LetConst } from './lesson_LetConst';
+// import { Lesson_Function } from './Lesson_Function';
+// import { Lesson_PrinAny } from './Lesson_PrinAny';
+import { Lesson_Optional } from './Lesson_Optional'
 
-Lesson_LetConst()
-Lesson_Function()
-Lesson_PrinAny()
+// Lesson_LetConst()
+// Lesson_Function()
+// Lesson_PrinAny()
+Lesson_Optional()
 
 
