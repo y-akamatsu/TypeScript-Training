2480d25cc18a322ce224848b3b3fad6e18fd91ce
y-akamatsu <ak1985tk@gmail.com>
2019-03-27 12:39:32 +0900

    modified Lesson_Static

--- beginner_lesson/src/Lesson_Static.ts
+++ beginner_lesson/src/Lesson_Static.ts
@@ -6,17 +6,19 @@
 */
 export function Lesson_Static() {
   const class1: LessonStatic1 = new LessonStatic1()
-  console.log(class1.getCount())
-
-  class1.setCount(200)
+  console.log(class1.getCount()) //LessonStatic1メソッドのgetCount()を呼び出す
+  
+  class1.setCount(300)
   console.log(class1.getCount())
 
   const class2: LessonStatic1 = new LessonStatic1()
-  console.log(class2.getCount())
-
+  console.log(class2.getCount()) 
+  
+  //クラスを生成しなくてもstaticを直接呼ぶことができる
   const count = LessonStatic1.count
   console.log(count)
-
+ 
+  //メソッドを直接呼ぶことができる
   const result = LessonStatic1.getResult()
   console.log(result)
 }
@@ -35,4 +37,3 @@ class LessonStatic1 {
 }
 
 //staticの使用方法
-//処理の流れが分からない
